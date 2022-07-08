ce4b59dc951a57425fbb121e8983d1211bc10be2
Ryan Decker <ryandecker2593@gmail.com>
2022-07-07 23:10:25 -0400

    Working test version going container -> container -> arduino -> hardware

--- hal/hal.py
+++ hal/hal.py
@@ -1,34 +1,79 @@
 #!/usr/bin/env python3
 
 # External Dependencies
+import RPi.GPIO as GPIO
+import serial
+
+import time
 import sys
 import socket
 import selectors
-import traceback
-
-# Local Dependencies
-import common.libserver
-
+import types
 
 sel = selectors.DefaultSelector()
+
+arduino = serial.Serial(port="/dev/ttyACM0", baudrate=115200, timeout=1)
+
+def write_read(val):
+    arduino.write(bytes(val, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readlines()
+    return data
+
+def blink_led():
+    # GPIO Pin setup
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(18, GPIO.OUT)
+
+    # Blink LED on and off 10 times
+    for i in range(0,10):
+        GPIO.output(18, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(18, GPIO.LOW)
+        time.sleep(1)
 
 
 def accept_wrapper(sock):
     conn, addr = sock.accept()  # Should be ready to read
     print(f"Accepted connection from {addr}")
     conn.setblocking(False)
-    message = libserver.Message(sel, conn, addr)
-    sel.register(conn, selectors.EVENT_READ, data=message)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
 
 
-if len(sys.argv) != 3:
-    print(f"Usage: {sys.argv[0]} <host> <port>")
-    sys.exit(1)
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+            # blink_led()
+            value = write_read("1")
+            print(value)
 
-host, port = sys.argv[1], int(sys.argv[2])
+
+
+
+
+# if len(sys.argv) != 3:
+#     print(f"Usage: {sys.argv[0]} <host> <port>")
+#     sys.exit(1)
+
+# host, port = sys.argv[1], int(sys.argv[2])
+host, port = "127.0.0.1", 44321
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Avoid bind() exception: OSError: [Errno 48] Address already in use
-lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 lsock.bind((host, port))
 lsock.listen()
 print(f"Listening on {(host, port)}")
@@ -42,15 +87,7 @@ try:
             if key.data is None:
                 accept_wrapper(key.fileobj)
             else:
-                message = key.data
-                try:
-                    message.process_events(mask)
-                except Exception:
-                    print(
-                        f"Main: Error: Exception for {message.addr}:\n"
-                        f"{traceback.format_exc()}"
-                    )
-                    message.close()
+                service_connection(key, mask)
 except KeyboardInterrupt:
     print("Caught keyboard interrupt, exiting")
 finally:
