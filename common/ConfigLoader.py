603f29f31b7d8bb18a4e398d6529b6721f47acd1
Ryan Decker <ryandecker2593@gmail.com>
2022-07-18 19:00:26 -0400

    Outline for config loader

--- common/ConfigLoader.py
+++ common/ConfigLoader.py
@@ -1,7 +1,21 @@
 import yaml
 
-with open("config.yaml", 'r') as stream:
+with open("/home/jhu-ep/InSECTS-Vehicle-Testbed/config.yaml", 'r') as stream:
     try:
         print(yaml.safe_load(stream))
     except yaml.YAMLError as exc:
         print(exc)
+
+
+class ConfigLoader():
+    def __init__(self) -> None:
+        pass
+
+    def __parse_params(self):
+        pass
+
+    def read_file(self):
+        pass
+
+    def get_config(self):
+        pass
