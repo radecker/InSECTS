c2505dd3399da7654116ea90ed8e7f30503c3d32
Ryan Decker <ryandecker2593@gmail.com>
2022-07-19 22:59:28 -0400

    Adding Hal service outline

--- hal_service/hal_service.py
+++ hal_service/hal_service.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python3
 
 from TCPServer import TCPServer
-from UDPClient import UDPClient
+from BaseApp import BaseApp
 import message_pb2 as proto
+import serial
 import time
 
+"""
+Purpose: This app is supposed to expose/emulate much of the hardware functions to the applications running
+
+Use Cases:
+    1. Abstract serial connection to arduino/wowki
+    2. ????
+"""
+
+class HalService(BaseApp):
+    def __init__(self) -> None:
+        self.arduino_addr = None
+        super().__init__(id="vehicle.hal_service")
+
+    def _send_to_arduino(self):
+        if self.config_params.emulate_arduino:
+            # Connect to Wowki server instead
+            pass
+        else:
+            # Send data over serial
+            pass
+
+    def setup(self):
+        self.arduino_addr = self.config_params.
+
+    def run(self):
+        pass
+
+    def shutdown(self):
+        pass
 
 
 if __name__ == "__main__":
-    udp_client = UDPClient(id="vehicle.hal_service")
-    udp_client.add_listener("224.1.1.1", 5050)
-    while True:
-        messages = udp_client.get_messages()
-        for msg in messages:
-            print(f"Message: {msg}")
+    HalService()
