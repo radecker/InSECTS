ac17b05e7728bcd0f06351288de7d6980705809d
Ryan Decker <ryandecker2593@gmail.com>
2022-07-19 21:42:24 -0400

    Implemented basic SDR app

--- sdr_app/sdr_app.py
+++ sdr_app/sdr_app.py
@@ -1,29 +1,40 @@
 #!/usr/bin/env python3
 
-from tcp_client import TCPClient
-from udp_client import UDPClient
+from BaseApp import BaseApp
+from TCPClient import TCPClient
 import message_pb2 as proto
 import time
+import queue
+
+"""
+Purpose: This app is used to connect the ground system to the vehicle via SDR or simulation
+"""
+
+class SDRApp(BaseApp):
+    def __init__(self, id: str) -> None:
+        self.tcp_client = None
+        self.tcp_send_queue = queue.Queue()
+        super().__init__(id)
+
+    def setup(self):
+        ip = self.config_params.sdr_tcp_client_ip
+        port = self.config_params.sdr_tcp_client_port
+        self.tcp_client = TCPClient(ip=ip, port=port, sender="vehicle.sdr_app")
+        self.tcp_client.start()
+
+    def run(self):
+        if len(self.command_queue):
+            for msg in self.command_queue:
+                if "ground." in msg.destination or "all" == msg.destination:
+                    self.tcp_client.send(msg=msg, dst=msg.destination)
+        if len(self.telemetry_queue):
+            for msg in self.telemetry_queue:
+                if "ground." in msg.destination or "all" == msg.destination:
+                    self.tcp_client.send(msg=msg, dst=msg.destination)
+
+    def shutdown(self):
+        pass
 
 
 if __name__ == "__main__":
-    tcp_client = TCPClient(ip="127.0.0.1", port=5052, sender="autonomy")
-    tcp_client.start()
-    udp_client = UDPClient(id="vehicle.sdr_app")
-    udp_client.add_listener("224.1.1.1", 5050)
-    udp_client.add_sender("224.1.1.1", 5050)
-    msg = proto.Message()
-    tcp_client.send(msg=msg, dst="sdr")
-    done = False
-    while True:
-        tcp_messages = tcp_client.get_messages()
-        udp_messages = udp_client.get_messages()
-        for msg in tcp_messages:
-            print(f"Received TCP: {msg}")
-            if not done:
-                time.sleep(2)
-                tcp_client.send(msg=msg, dst="gui")
-                udp_client.send(msg=msg, group="224.1.1.1", port=5050)
-                done = True
-        for msg in udp_messages:
-            print(f"Received UDP: {msg}")
+    SDRApp()
