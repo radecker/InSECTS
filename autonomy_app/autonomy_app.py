976b8898d2cfcb71435175546b2df4c089efa14a
Ryan Decker <ryandecker2593@gmail.com>
2022-08-02 19:47:24 -0400

    Modified autonomy to work with infrastracture

--- autonomy_app/autonomy_app.py
+++ autonomy_app/autonomy_app.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-#from TCPClient import TCPClient
-#from UDPClient import UDPClient
-#from BaseApp import BaseApp
-#import message_pb2 as proto
+import queue
+from BaseApp import BaseApp
+import message_pb2 as proto
 import time
+import statistics
+# import csv
+# import numpy as np
+
 
 # Autonomy Rules
 #
@@ -41,108 +44,118 @@ import time
 # 1 output every second from each sensor 1 through 5
 
 
-
-import csv
-import numpy as np
-
-class AutonomyApp ():
+class AutonomyApp (BaseApp):
     def __init__(self) -> None:
-        #super().__init__("vehicle.autonomy_app")
-        self.baffle = 0 #0 is closed 1 is half 2 is open
-        self.fanspeed = 1 # 0 is off 1 is 25% 2 is 50% 3 is 75% 4 is 100%
-        self.csv = r"C:\Users\skido\OneDrive\Desktop\Grad School\DRAGONFLYCUBE-SAT\Autonomy_Script_Test.csv"
+        self.baffle = 0     # 0 is closed 1 is half 2 is open
+        self.fanspeed = 1   # 0 is off 1 is 25% 2 is 50% 3 is 75% 4 is 100%
+        self.manual_mode = None
+        self.temp_data = dict()
+        super().__init__("vehicle.autonomy_app")
         
-    def read_tempSensors(self): 
-        tempSensors = []
-        AveSingeSensorTemp = []
-        with open(self.csv,mode = 'r')as file:
-            csvFile = csv.reader(file)
-            counter =0 
-            for lines in csvFile:
-                if counter ==0:
-                    counter =1 
-                else : # Takes last 5 seconds of data for max & average
-                    l=len(lines)
-                    AveSingeSensorTemp.append(int(lines[l-5]))
-                    AveSingeSensorTemp.append(int(lines[l-4]))
-                    AveSingeSensorTemp.append(int(lines[l-3]))
-                    AveSingeSensorTemp.append(int(lines[l-2]))
-                    AveSingeSensorTemp.append(int(lines[l-1]))
-                    tempSensors.append(np.mean (AveSingeSensorTemp))
-                    AveSingeSensorTemp = []
-            return tempSensors #return array 
+    def read_tempSensors(self):
+        for msg in self.telemetry_queue:
+            if msg.HasField("telemetry"):
+                if msg.telemetry.HasField("temperature_data"):
+                    sensor_id = msg.telemetry.temperature_data.sensor_id
+                    if sensor_id not in self.temp_data:
+                        self.temp_data[sensor_id] = []
+                        self.temp_data[sensor_id].append(msg.telemetry.temperature_data.sensor_value)
+                    elif len(self.temp_data[sensor_id]) >= 5:
+                        self.temp_data[sensor_id].pop(0)
+                        self.temp_data[sensor_id].append(msg.telemetry.temperature_data.sensor_value)
+                    else:
+                        self.temp_data[sensor_id].append(msg.telemetry.temperature_data.sensor_value)
+        # tempSensors = []
+        # AveSingeSensorTemp = []
+        # with open(self.csv,mode = 'r') as file:
+        #     csvFile = csv.reader(file)
+        #     counter = 0 
+        #     for lines in csvFile:
+        #         if counter == 0:
+        #             counter = 1 
+        #         else : # Takes last 5 seconds of data for max & average
+        #             l=len(lines)
+        #             AveSingeSensorTemp.append(int(lines[l-5]))
+        #             AveSingeSensorTemp.append(int(lines[l-4]))
+        #             AveSingeSensorTemp.append(int(lines[l-3]))
+        #             AveSingeSensorTemp.append(int(lines[l-2]))
+        #             AveSingeSensorTemp.append(int(lines[l-1]))
+        #             tempSensors.append(statistics.mean(AveSingeSensorTemp))
+        #             AveSingeSensorTemp = []
+        #     return tempSensors #return array 
         
     def send_fanspeed_and_baffle(self):
-        if self.fanspeed ==1 :
-            #Function to send 1 to ardunio (xxx volts)
-            print(self.fanspeed)
-        elif self.fanspeed ==2 :
-            #Function to send 2 to ardunio (xxx volts)
-            print(self.fanspeed)
-        elif self.fanspeed ==3 :
-            #Function to send 3 to ardunio (xxx volts)
-            print(self.fanspeed)
-        elif self.fanspeed ==4 :
-            #Function to send 4 to ardunio (xxx volts)
-            print(self.fanspeed)
-            
-        if self.baffle == 0:
-            #Function to send 0 to baffle XXX
-            print(self.baffle)
-        elif self.baffle ==1:
-            #Function to send 1 to baffle XXX
-            print(self.baffle)
-        elif self.baffle ==2:
-            #Function to send 2 to baffle XXX
-            print(self.baffle)
-            
-    def run(self):
-        print("Autonomy System Online")
-        self.send_fanspeed_and_baffle()
-        time.sleep (10)
-        while True:
-            allTemps =self.read_tempSensors() #value return stored in all temps
-            if np.mean(allTemps) > 95 and self.fanspeed==1: 
-                self.fanspeed=2
-            elif np.mean(allTemps) > 100 and self.fanspeed==2: 
-                self.fanspeed=3
-            elif np.mean(allTemps) > 105 and self.fanspeed==3: 
-                self.fanspeed=4
-            elif np.mean(allTemps) < 95 and self.fanspeed >=2: 
-                self.fanspeed=1
-            elif np.mean(allTemps) < 100 and self.fanspeed >=3: 
-                self.fanspeed=2
-            elif np.mean(allTemps) < 105 and self.fanspeed ==4: 
-                self.fanspeed=3
-                     
-            if np.max(allTemps) > 100 and self.fanspeed==1:
-                self.fanspeed=2
-            elif np.max(allTemps) > 105 and self.fanspeed==2:
-                self.fanspeed=3
-            elif np.max(allTemps) > 110 and self.fanspeed==3:
-                self.fanspeed=4
-            elif np.max(allTemps) < 100 and self.fanspeed >=2:
-                self.fanspeed=1
-            elif np.max(allTemps) < 105 and self.fanspeed >=3:
-                self.fanspeed=2
-            elif np.max(allTemps) < 110 and self.fanspeed ==4:
-                self.fanspeed=3
-    
-            if (allTemps[0] > 50 or allTemps[1] > 50) and self.baffle ==0: 
-                self.baffle =1
-            elif (allTemps[0] > 55 or allTemps[1] > 55) and self.baffle ==1: 
-                self.baffle =2
-            elif (allTemps[0] < 50 or allTemps[1] < 50) and self.baffle >=1: 
-                 self.baffle =0
-            elif (allTemps[0] < 55 or allTemps[1] < 55) and self.baffle ==2: 
-                 self.baffle =1
-                 
-            self.send_fanspeed_and_baffle()
-            time.sleep(30)
-            
+        msg = proto.Message()
+        cmd = proto.Command()
+        fan_cmd = proto.SetFanSpeed()
+        fan_cmd.fan_speed = self.fanspeed
+        cmd.set_fan_speed.CopyFrom(fan_cmd)
+        msg.command.CopyFrom(cmd)
+        print(msg)
 
+        msg = proto.Message()
+        cmd = proto.Command()
+        servo_cmd = proto.SetServoPosition()
+        servo_cmd.servo_pos = self.baffle
+        cmd.set_servo_position.CopyFrom(servo_cmd)
+        msg.command.CopyFrom(cmd)
+        print(msg)
+            
+    def setup(self) -> None:
+        self.manual_mode = False
+        print("Autonomy System Online")
+    
+    def run(self) -> None:
+        # Read commands and telem
+        if len(self.command_queue):
+            pass
+        if len(self.telemetry_queue):
+            self.read_tempSensors()
+        if not self.manual_mode:
+            alltemps = []
+            for sensor in self.temp_data.values():
+                for temp in sensor:
+                    alltemps.append(temp)
+            if len(alltemps) > 2:
+                if statistics.mean(alltemps) > 95 and self.fanspeed == 25: 
+                    self.fanspeed = 50
+                elif statistics.mean(alltemps) > 100 and self.fanspeed == 50: 
+                    self.fanspeed = 75
+                elif statistics.mean(alltemps) > 105 and self.fanspeed == 75: 
+                    self.fanspeed = 100
+                elif statistics.mean(alltemps) < 95 and self.fanspeed >= 50: 
+                    self.fanspeed = 25
+                elif statistics.mean(alltemps) < 100 and self.fanspeed >= 75: 
+                    self.fanspeed = 50
+                elif statistics.mean(alltemps) < 105 and self.fanspeed == 100: 
+                    self.fanspeed = 75
+                        
+                if max(alltemps) > 100 and self.fanspeed == 25:
+                    self.fanspeed = 50
+                elif max(alltemps) > 105 and self.fanspeed == 50:
+                    self.fanspeed = 75
+                elif max(alltemps) > 110 and self.fanspeed == 75:
+                    self.fanspeed = 100
+                elif max(alltemps) < 100 and self.fanspeed >= 50:
+                    self.fanspeed = 25
+                elif max(alltemps) < 105 and self.fanspeed >= 75:
+                    self.fanspeed = 50
+                elif max(alltemps) < 110 and self.fanspeed == 100:
+                    self.fanspeed = 75
+
+                if (alltemps[0] > 50 or alltemps[1] > 50) and self.baffle == 0: 
+                    self.baffle = 1
+                elif (alltemps[0] > 55 or alltemps[1] > 55) and self.baffle == 1: 
+                    self.baffle = 2
+                elif (alltemps[0] < 50 or alltemps[1] < 50) and self.baffle >= 1: 
+                        self.baffle = 0
+                elif (alltemps[0] < 55 or alltemps[1] < 55) and self.baffle == 2: 
+                        self.baffle = 1
+                    
+                self.send_fanspeed_and_baffle()
+                time.sleep(5)
 
 if __name__ == "__main__":
     a= AutonomyApp()
-    a.run() # Runs the app
+    # a.run() # Runs the app
     
--- embedded/arduino_controller/arduino_controller.ino
+++ embedded/arduino_controller/arduino_controller.ino
@@ -5,20 +5,22 @@ String serial_in;
 const uint8_t led_pin = 8;
 
 // Global temp variables
-uint8_t temp_sensors[] = {A0, A1};
-double prev_temps[] = {0.0, 0.0};
-double temps[] = {0.0, 0.0};
+uint8_t temp_sensors[] = {A0, A1}; // , A2, A3, A4, A5};
+double prev_temps[] = {0.0, 0.0}; //, 0.0, 0.0, 0.0, 0.0};
+double temps[] = {0.0, 0.0}; //, 0.0, 0.0, 0.0, 0.0};
 uint8_t num_temp_sensors = 2;
 
 static unsigned long TELEMETRY_PERIOD = 1000; // 1000 ms default between telemetry grabs
 
-// Servo Global variables
-int fullpos = 0;
-int halfpos = 90;
-int closepos = 180;
-int currentservopos = 180;
+// Servo & Fan Global variables
+int fullpos = 0; // ***Update based on final model setup!
+int halfpos = 90; // ***Update based on final model setup!
+int closepos = 180; // ***Update based on final model setup!
+int currentservopos = 180; // ***Update based on final model setup!
+int fan_speed = 0;
+int fan_level = 0;
 int fancontrol_pin = 11;
-int fanfeedback_pin = 2; //define the interrupt pin (must be pin 2 or 3)
+const int fanfeedback_pin = 2; // define the interrupt pin (must be pin 2 or 3)
 int InterruptCounter;
 
 double updateTempData(uint8_t sensor_id);
@@ -32,7 +34,8 @@ void setup() {
   pinMode(led_pin, OUTPUT);
   pinMode(fancontrol_pin, INPUT);
   myservo.attach(9); // attaches the servo on pin 9 to the servo object
-  myservo.write(closepos); // initial servo position
+  myservo.write(currentservopos);
+  analogWrite(fancontrol_pin,fan_level);
 }
 
 void loop() {
@@ -58,7 +61,7 @@ void loop() {
 
 void runCommand(String serial)
 {
-  static uint8_t cmd = serial.substring(0,1).toInt(); // Grabs just the first character
+  uint8_t cmd = serial.substring(0,1).toInt(); // Grabs just the first character
   String args = serial.substring(2); // Grabs everything past the first comma
   if(cmd == 0)  // Config Parameters
   {
@@ -67,42 +70,62 @@ void runCommand(String serial)
   }
   if(cmd == 1)  // SetServoPosition
   {
-    String servo_pos = args.substring(0,4); // Grabs the first substring within "args" string
-    if (servo_pos == "full")
+    uint8_t servo_pos = args.substring(0,3).toInt(); // Grabs the first substring within "args" string
+    if (servo_pos == 2)
     {
       myservo.write(fullpos);
-      currentservopos = 0;
+      currentservopos = 0; // this will be adjusted once final model is built
+      Serial.println("full cmd received");
     }
-    else if (servo_pos == "half")
+    else if (servo_pos == 1)
     {
       myservo.write(halfpos);
-      currentservopos = 90;
+      currentservopos = 90; // this will be adjusted once final model is built
+      Serial.println("half cmd received");
     }
-    else if (servo_pos == "clse")
+    else if (servo_pos == 0)
     {
       myservo.write(closepos);
-      currentservopos = 180;
+      currentservopos = 180; // this will be adjusted once final model is built
+      Serial.println("clse cmd received");
     } 
   }
   if(cmd == 2)  // SetFanSpeed
   {
-    static uint8_t fan_speed = args.substring(0,3).toInt(); // Grabs the first substring within "args" string
-    fan_speed = map(fan_speed, 0, 100, 0, 255);
-    analogWrite(fancontrol_pin,fan_speed);
-  }
-  if(cmd == 3)  // SetFanState
-  {
-    //**On or off
+    int fan_speed = args.substring(0,3).toInt(); // Grabs the first substring within "args" string
+    fan_level = map(fan_speed, 0, 100, 0, 255); // Below 40% the fanspeed doesn't seem to change
+    analogWrite(fancontrol_pin,fan_level);
+    // Serial.println("fan cmd received"); TODO: remove when done testing
   }
 }
 
 void sendTelemetry()
 {
-  static double temp = 0;
+  // Telemetry is sent in a csv format and parsing must be implemented in the HAL
+  // E.g. temp = 1,26.30 translates into telemetry type 1 and 26.30 degrees C
+  
+  // Send temperature data
+  double temp = 0;
   for(int i = 0; i < num_temp_sensors; i++){
-    String serial_data = "1," + String(i) + "," + String(temps[i], 2);
+    String serial_data = "0," + String(i) + "," + String(temps[i], 2);
     Serial.println(serial_data);
   }
+/*
+  // Send servo position
+  String serial_servo = "1," + String(currentservopos);
+  Serial.println(serial_servo); // Send servomotor position
+
+  // Send the current fan speed
+  double fanspeed;
+  // Interrupt counter to read tachometer signal pulses
+  InterruptCounter = 0;
+  attachInterrupt(digitalPinToInterrupt(fanfeedback_pin), counter, RISING);
+  delay(500); // This pauses all execution, we need to swap out with non blocking version or reduce time
+  detachInterrupt(digitalPinToInterrupt(fanfeedback_pin));
+  fanspeed = (InterruptCounter / 2) * 120; // calculates fan speed from tachometer pulses
+  String serial_fan = "2," + String(fanspeed);
+  Serial.println(serial_fan); // Send fan speed
+*/
 }
 
 void refreshTelemetryData(){
@@ -113,22 +136,11 @@ void refreshTelemetryData(){
 double updateTempData()
 {
   for(int i = 0; i < num_temp_sensors; i++){
-    temps[i] = (analogRead(temp_sensors[i])*(5000/1024.0) + prev_temps[i])/2; // Averaging filter
+    temps[i] = analogRead(temp_sensors[i])*(5000/1024.0);  // Averaging filter
     temps[i] = (temps[i]-110)/10;     // 110 is magic offset for TMP35
     prev_temps[i] = temps[i];
+    temps[i] = (temps[i] + prev_temps[i])/2.0;
   }
-}
-
-double getFanSpeed()
-{
-  // TODO: Get this into telemetry serial string
-  double fanspeed;
-  InterruptCounter = 0;
-  attachInterrupt(digitalPinToInterrupt(fanfeedback_pin), counter, RISING);
-  delay(1000);
-  detachInterrupt(digitalPinToInterrupt(fanfeedback_pin));
-  fanspeed = (InterruptCounter / 2) * 60;
-  return fanspeed;
 }
 
 void counter()
